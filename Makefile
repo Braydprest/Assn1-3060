467cae6d4981c7399641266b3089465c9978bfdf
Brayden P <J_10529668@cs3060.ad.uvu.edu>
2015-09-05 17:33:45 -0600

    test

---
+++ Makefile
@@ -0,0 +1,2 @@
+assn1: assn1.c Makefile
+	gcc -g -Werror -Wextra -Wall -o ass1 assn1.c
---
+++ assn1.c
@@ -0,0 +1,20 @@
+// Brayden Prestwich
+// CS 3060 - 001 Fall 2015
+// Assignment 1
+/* Promise of Originality
+Ipromise that this source code file has in it's entirety, been
+written by myself and by no other person or Persons. If at any time an
+exact copy of this source code is found to be used by another person in
+this term, I understand that both myself and the student that submitted
+the copy will receive a zero on this assignment
+*/
+
+
+include <stdio.h>
+
+int main() {
+	int argc
+	char *argv[];
+	printf("Hello World\n");
+	return 0;
+}
